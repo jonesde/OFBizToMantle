836c8c64303f08237a6e56ab6866fc42ca50bc71
David E Jones <dej@dejc.com>
2017-02-14 22:08:58 -0800

    Added transformers for other party entities for UserLogin

--- src/main/groovy/mantle/ofbiz/OFBizFieldMap.groovy
+++ src/main/groovy/mantle/ofbiz/OFBizFieldMap.groovy
@@ -20,9 +20,35 @@ class OFBizFieldMap {
     static String map(String field, String value) {
         LinkedHashMap<String, String> valueMap = fieldValueMaps.get(field)
         if (valueMap == null) return value
-        return valueMap.get(value) ?: value
+        return valueMap.containsKey(value) ? valueMap.get(value) : value
+    }
+    static String get(String field, String value) {
+        LinkedHashMap<String, String> valueMap = fieldValueMaps.get(field)
+        if (valueMap == null) return null
+        return valueMap.get(value)
     }
     static LinkedHashMap<String, LinkedHashMap<String, String>> fieldValueMaps = [
-        partyTypeId:[PERSON:'PtyPerson', PARTY_GROUP:'PtyOrganization']
+        partyTypeId:[PERSON:'PtyPerson', PARTY_GROUP:'PtyOrganization'],
+        roleTypeId:['_NA_':null, ACCOUNT:'Account', ACCOUNT_LEAD:'AccountLead', LEAD:'AccountLead', AGENT:'Agent',
+                AFFILIATE:'Affiliate', CARRIER:'Carrier', COMPETITOR:'Competitor', CONSUMER:'Consumer', CONTRACTOR:'Contractor',
+                DISTRIBUTOR:'Distributor', ISP:'InternetSp', HOSTING_SERVER:'HostingProvider', MANUFACTURER:'Manufacturer',
+                OWNER:'Owner', PARTNER:'Partner', PROSPECT:'Prospect', REFERRER:'Referrer', SHAREHOLDER:'Shareholder',
+                SUBSCRIBER:'Subscriber', SUPPLIER:'Supplier', VISITOR:'Visitor', WEB_MASTER:'WebMaster',
+                ACCOUNTANT:'Accountant', ADMIN:'Administrator', BUYER:'Buyer', CASHIER:'Cashier', CLIENT_BILLING:'ClientBilling',
+                CLIENT_MANAGER:'ClientManager', CONTACT:'Contact', EMAIL_ADMIN:'EmailAdmin', EMPLOYEE:'Employee',
+                FAMILY_MEMBER:'FamilyMember', MANAGER:'Manager', ORDER_CLERK:'OrderClerk', PACKER:'Packer',
+                PICKER:'Picker', RECEIVER:'Receiver', SALES_REP:'SalesRepresentative', SHIPMENT_CLERK:'ShipmentClerk',
+                SPONSOR:'Sponsor', SPOUSE:'Spouse', STOCKER:'Stocker', SUPPLIER_AGENT:'SupplierAgent', WORKER:'Worker',
+                ASSOCIATION:'OrgAssociation', DEPARTMENT:'OrgDepartment', DIVISION:'OrgDivision', HOUSEHOLD:'OrgHousehold',
+                INTERNAL_ORGANIZATIO:'OrgInternal', PROJECT_TEAM:'OrgTeam', SCRUM_TEAM:'OrgTeam', ORGANIZATION_UNIT:'OrgUnit',
+                PARENT_ORGANIZATION:'OrgParent', REGULATORY_AGENCY:'OrgRegulatoryAgency', SUBSIDIARY:'OrgSubsidiary',
+                TAX_AUTHORITY:'OrgTaxAuthority', UNION:'OrgUnion',
+                CUSTOMER:'Customer', BILL_TO_CUSTOMER:'CustomerBillTo', END_USER_CUSTOMER:'CustomerEndUser',
+                PLACING_CUSTOMER:'CustomerPlacing', SHIP_TO_CUSTOMER:'CustomerShipTo', VENDOR:'Vendor',
+                BILL_FROM_VENDOR:'VendorBillFrom', SHIP_FROM_VENDOR:'VendorShipFrom'],
+        partyRelationshipTypeId:[ACCOUNT:'PrtCustomer', AGENT:'PrtAgent', CHILD:'PrtChild', CONTACT_REL:'PrtContact',
+                CUSTOMER_REL:'PrtCustomer', EMPLOYMENT:'PrtEmployee', FRIEND:'PrtFriend', GROUP_ROLLUP:'PrtOrgRollup',
+                LEAD_OWNER:'PrtCustomer', MANAGER:'PrtManager', SALES_AFFILIATE:'PrtSalesAffiliate', SALES_REP:'PrtRepresentative',
+                SPOUSE:'PrtSpouse', SUPPLIER_REL:'PrtSupplier']
     ]
 }
--- src/main/groovy/mantle/ofbiz/OFBizTransform.groovy
+++ src/main/groovy/mantle/ofbiz/OFBizTransform.groovy
@@ -14,6 +14,8 @@
 package mantle.ofbiz
 
 import groovy.transform.CompileStatic
+import org.moqui.Moqui
+import org.moqui.context.ExecutionContext
 import org.moqui.etl.SimpleEtl
 import org.moqui.etl.SimpleEtl.EntryTransform
 import org.moqui.etl.SimpleEtl.SimpleEntry
@@ -22,31 +24,110 @@ import static mantle.ofbiz.OFBizFieldMap.map
 
 @CompileStatic
 class OFBizTransform {
-    static List<String> loadOrder = ['Party', 'Person', 'PartyGroup']
+    static List<String> loadOrder = ['Party', 'Person', 'PartyGroup', 'PartyRole', 'PartyClassification', 'PartyRelationship', 'UserLogin'
+            /*'ContactMech', 'PartyContactMechPurpose', 'PostalAddress', 'TelecomNumber'*/]
+    static List<List<String>> loadOrderParallel = [
+            ['Party', 'ContactMech'],
+            ['Person', 'PartyGroup', 'PartyRole', 'UserLogin', 'PartyContactMechPurpose', 'PostalAddress', 'TelecomNumber'],
+            ['PartyClassification', 'PartyRelationship']]
+
     static SimpleEtl.TransformConfiguration conf = new SimpleEtl.TransformConfiguration()
     static {
         conf.addTransformer("Party", new Transformer() { void transform(EntryTransform et) { Map<String, Object> val = et.entry.etlValues
             et.addEntry(new SimpleEntry("mantle.party.Party", [partyId:val.partyId, partyTypeEnumId:map('partyTypeId', (String) val.partyTypeId),
-                externalId:val.externalId, disabled:("PARTY_DISABLED".equals(val.statusId) ? 'N' : 'Y'), lastUpdatedStamp:val.lastUpdatedTxStamp]))
+                externalId:val.externalId, disabled:("PARTY_DISABLED".equals(val.statusId) ? 'N' : 'Y'), lastUpdatedStamp:((String) val.lastUpdatedTxStamp).take(23)]))
         }})
         conf.addTransformer("PartyGroup", new Transformer() { void transform(EntryTransform et) { Map<String, Object> val = et.entry.etlValues
             et.addEntry(new SimpleEntry("mantle.party.Organization", [partyId:val.partyId, organizationName:((String) val.groupName)?.trim(),
                 officeSiteName:(!val.officeSiteName || ((String) val.officeSiteName).toLowerCase() == ((String) val.groupName).toLowerCase() ? '' : val.officeSiteName),
-                annualRevenue:val.annualRevenue, numEmployees:val.numEmployees, lastUpdatedStamp:val.lastUpdatedTxStamp]))
+                annualRevenue:val.annualRevenue, numEmployees:val.numEmployees, lastUpdatedStamp:((String) val.lastUpdatedTxStamp).take(23)]))
         }})
+        // TODO: how to handle PartyNote combined with NoteData? split easy, combine another question
+        conf.addTransformer("PartyRelationship", new Transformer() { void transform(EntryTransform et) { Map<String, Object> val = et.entry.etlValues
+            // TODO: should from/to be reversed? relationship types and approach varies somewhat so some, but not all, may need reversing
+            et.addEntry(new SimpleEntry("mantle.party.PartyRelationship", [partyRelationshipId:UUID.randomUUID().toString(),
+                    relationshipTypeEnumId:map('partyRelationshipTypeId', (String) val.partyRelationshipTypeId),
+                    fromPartyId:val.partyIdFrom, fromRoleTypeId:map('roleTypeId', (String) val.roleTypeIdFrom),
+                    toPartyId:val.partyIdTo, toRoleTypeId:map('roleTypeId', (String) val.roleTypeIdTo),
+                    fromDate:val.fromDate, thruDate:val.thruDate, comments:val.comments, relationshipName:val.relationshipName,
+                    lastUpdatedStamp:((String) val.lastUpdatedTxStamp).take(23)]))
+            // NOTE: doesn't map statusId, maybe do in future if ever used
+        }})
+        conf.addTransformer("PartyRole", new Transformer() { void transform(EntryTransform et) { Map<String, Object> val = et.entry.etlValues
+            String roleTypeId = val.roleTypeId
+            // handle _NA_ role which is a placeholder in OFBiz and never needed (should be eliminated) in Moqui
+            if ("_NA_".equals(roleTypeId)) { et.loadCurrent(false); return }
+            String rtMapped = OFBizFieldMap.get('roleTypeId', roleTypeId)
+            if (rtMapped == null) {
+                ExecutionContext ec = Moqui.getExecutionContext()
+                if (ec.entity.find("mantle.party.RoleType").condition("roleTypeId", roleTypeId).useCache(true).one() == null) {
+                    et.addEntry(new SimpleEntry("mantle.party.RoleType", [roleTypeId:roleTypeId,
+                            description:roleTypeId.split("_").collect({ ((String) it).toLowerCase().capitalize() }).join(" ")] as Map<String, Object>))
+                }
+                rtMapped = roleTypeId
+            }
+            et.addEntry(new SimpleEntry("mantle.party.PartyRole", [partyId:val.partyId, roleTypeId:rtMapped, lastUpdatedStamp:((String) val.lastUpdatedTxStamp).take(23)]))
+        }})
+        
         conf.addTransformer("Person", new Transformer() { void transform(EntryTransform et) {
             Map<String, Object> val = et.entry.etlValues
             String firstName = ((String) val.firstName)?.trim()?.capitalize()
             String middleName = ((String) val.middleName)?.trim()?.capitalize()
             String lastName = ((String) val.lastName)?.trim()?.capitalize()
             Map<String, Object> entryMap = new HashMap<>(val) // most fields match name, rest will be ignored
-            entryMap.putAll([firstName:firstName, middleName:middleName, lastName:lastName, lastUpdatedStamp:val.lastUpdatedTxStamp])
+            entryMap.putAll([firstName:firstName, middleName:middleName, lastName:lastName, lastUpdatedStamp:((String) val.lastUpdatedTxStamp).take(23)])
             et.addEntry(new SimpleEntry("mantle.party.Person", entryMap))
+            // NOTE could add PartyIdentification records for socialSecurityNumber, passportNumber/passportExpireDate
         }})
 
+
+        conf.addTransformer("UserLogin", new Transformer() { void transform(EntryTransform et) {
+            Map<String, Object> val = et.entry.etlValues
+            String currentPassword = (String) val.currentPassword
+            String passwordHashType = null
+            if (currentPassword != null) {
+                int hashEnd = -1
+                if (currentPassword.startsWith('$')) hashEnd = currentPassword.indexOf('$', 1)
+                else if (currentPassword.startsWith('{')) hashEnd = currentPassword.indexOf('}', 1)
+                if (hashEnd > 0) {
+                    passwordHashType = currentPassword.substring(1, hashEnd)
+                    currentPassword = currentPassword.substring(hashEnd + 1)
+                }
+            }
+            et.addEntry(new SimpleEntry("moqui.security.UserAccount", [userId:val.userLoginId, username:val.userLoginId,
+                partyId:val.partyId, passwordHint:val.passwordHint, requirePasswordChange:val.requirePasswordChange,
+                disabled:(val.enabled == 'N' ? 'Y' : 'N'), disabledDateTime:val.disabledDateTime, successiveFailedLogins:val.successiveFailedLogins,
+                currencyUomId:val.lastCurrencyUom, locale:val.lastLocale, timeZone:val.lastTimeZone,
+                currentPassword:currentPassword, passwordHashType:passwordHashType, passwordSetDate:((String) val.lastUpdatedTxStamp).take(23),
+                lastUpdatedStamp:((String) val.lastUpdatedTxStamp).take(23)]))
+        }})
 
         /*
 
          */
+
+        // =========== Custom Transformers ===========
+        conf.addTransformer("PartyClassification", new Transformer() { void transform(EntryTransform et) {
+            Map<String, Object> val = et.entry.etlValues
+            String partyClassificationGroupId = val.partyClassificationGroupId
+            if (customerClasses.containsKey(partyClassificationGroupId)) {
+                et.addEntry(new SimpleEntry("mantle.party.PartyClassificationAppl", [partyId:val.partyId,
+                        partyClassificationId:customerClasses.get(partyClassificationGroupId),
+                        fromDate:val.fromDate, thruDate:val.thruDate, lastUpdatedStamp:((String) val.lastUpdatedTxStamp).take(23)]))
+            } else if (settlementTermByClass.containsKey(partyClassificationGroupId)) {
+                String agreementId = (String) val.partyId + '_' + partyClassificationGroupId
+                et.addEntry(new SimpleEntry("mantle.party.agreement.Agreement", [agreementId:agreementId, agreementTypeEnumId:'AgrSales',
+                        organizationPartyId:'Company', otherPartyId:val.partyId, otherRoleTypeId:'Customer', description:'Customer Terms',
+                        agreementDate:val.fromDate, fromDate:val.fromDate, thruDate:val.thruDate, lastUpdatedStamp:((String) val.lastUpdatedTxStamp).take(23)]))
+                et.addEntry(new SimpleEntry("mantle.party.agreement.AgreementTerm", [agreementTermId:agreementId+'_CT', agreementId:agreementId,
+                        settlementTermId:settlementTermByClass.get(partyClassificationGroupId), lastUpdatedStamp:((String) val.lastUpdatedTxStamp).take(23)] as Map<String, Object>))
+            } else {
+                et.loadCurrent(false)
+            }
+        }})
     }
+
+    // =========== Custom Mappings ===========
+    static Map<String, String> customerClasses = ['10000':'CustWholesale', '10001':'CustDistributor', '10002':'CustEnterprise']
+    static Map<String, String> settlementTermByClass = ['10010':'Net45Disc2Pct15', 'PCG_CASH':'Immediate', 'PCG_NET30':'Net30']
 }
